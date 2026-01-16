d1dd6081ffcfab99ba675060beab2b6f929cdf64
aaltaf65 <aqueelnazim65@gmail.com>
2026-01-16 13:16:49 -0500

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    :param file_path:
+    :return: List of requirements
+    '''
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.1.0",
+    author="Aqueel",
+    author_email='aqueelnazim65@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
